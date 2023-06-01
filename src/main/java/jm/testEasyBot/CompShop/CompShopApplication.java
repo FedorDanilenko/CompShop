90d6e76ec7493e2c5344bf52c17ff0b43b626af7
Fedor Danilenko <divine.world.mail@gmail.com>
2023-06-01 19:48:04 +0400

    Create project

---
+++ src/main/java/jm/testEasyBot/CompShop/CompShopApplication.java
@@ -0,0 +1,13 @@
+package jm.testEasyBot.CompShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompShopApplication.class, args);
+	}
+
+}
